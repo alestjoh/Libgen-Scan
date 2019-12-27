cf2539a3c6972405d960acbf49595b0c07af312e
Lyndis <alestjoh@gmail.com>
2019-12-26 18:10:11 -0700

    Begin updating plugins from Maven to jCenter

--- app/src/main/java/com/nfd/libgenscan/openLibrary/BookData.kt
+++ app/src/main/java/com/nfd/libgenscan/openLibrary/BookData.kt
@@ -1,5 +1,7 @@
 package com.nfd.libgenscan.openLibrary
 
+import android.os.Parcelable
+
 /**
  * This structure matches the response JSON object from Open Library. This is not a complete
  * set of all fields returned in BookData, though. Other fields may be added as necessary.
@@ -8,12 +10,13 @@ data class BookResponse(
         val bookData: BookData
 )
 
+//@Parcelize
 data class BookData(
         val url: String?,
         val title: String?,
         val subtitle: String?,
         val authors: List<Author>?
-)
+)//: Parcelable
 
 data class Author(
         val name: String,
